6592823c4141a2c6f9fbb563456abed145d6df2f
NgoNhatAnhTrung <ngonhatanhtrung@gmail.com>
2022-10-08 23:50:11 +0700

    first commit

---
+++ app/src/main/java/com/example/profile_ltdtdd/Home.java
@@ -0,0 +1,67 @@
+package com.example.profile_ltdtdd;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Home extends AppCompatActivity {
+
+    private BottomNavigationView mNavigationView;
+    private ViewPager mViewPager;
+
+    Button btL,btPf;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        getSupportActionBar().hide(); //This line hides the acction bar
+        setContentView(R.layout.activity_home);
+
+        mNavigationView = findViewById(R.id.bottom_nav);
+        mViewPager = findViewById(R.id.view_pager);
+
+        setUpViewPager();
+
+        mNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.action_home:
+                        mViewPager.setCurrentItem(0);
+                        break;
+
+                    case R.id.action_list:
+                        mViewPager.setCurrentItem(1);
+                        break;
+
+                    case R.id.action_profile:
+                        mViewPager.setCurrentItem(2);
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    private void setUpViewPager() {
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        mViewPager.setAdapter(viewPagerAdapter);
+
+    }
+
+}
